128ae2bcff6d16a1dbe061f0195b13590830d9c9
Gar|k <garik.djan@gmail.com>
2020-03-20 00:19:13 +0300

    feat: create new data

--- main.go
+++ main.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 	"github.com/eoscanada/eos-go"
 
-	"github.com/jackc/pgx/v4"
 	"context"
+	"github.com/jackc/pgx/v4"
 	"log"
 	"os"
 	"os/signal"
@@ -15,11 +15,11 @@ import (
 
 const (
 	contractAbiFileName = "contract.abi"
-	contractActionName = "send"
-	dataSource = "postgres://test:test@localhost/test"
-	interval = time.Millisecond * 500
-	sqlTruncate = "TRUNCATE TABLE chain.action_trace"
-	sqlInsert = "INSERT INTO chain.action_trace(transaction_id, action_ordinal, act_name, act_data, block_num, receipt_global_sequence) VALUES ('A229C41BF5974D45E2EB11D9987B92E980C68AF9A0C170F71CFF868469EF3DC5',1,'send', $1, $2, $3)"
+	contractActionName  = "send"
+	dataSource          = "postgres://test:test@localhost/test"
+	interval            = time.Millisecond * 500
+	sqlTruncate         = "TRUNCATE TABLE chain.action_trace"
+	sqlInsert           = "INSERT INTO chain.action_trace(transaction_id, action_ordinal, act_name, act_data, block_num, receipt_global_sequence) VALUES ('A229C41BF5974D45E2EB11D9987B92E980C68AF9A0C170F71CFF868469EF3DC5',1,'send', $1, $2, $3)"
 )
 
 func main() {
@@ -54,21 +54,18 @@ func main() {
 
 	increment = 0
 
-	var data []byte
-	data, err = encodeSendAction(abi, 0)
-
 	done := make(chan os.Signal, 1)
 	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 
 	for {
 		select {
 		case <-ticker.C:
-			_, err = conn.Exec(context.Background(), sqlInsert, data, increment, increment)
-			if err != nil {
-				log.Fatal(err)
+			if data, err := encodeSendAction(abi, 0); err == nil {
+				if _, err := conn.Exec(context.Background(), sqlInsert, data, increment, increment); err == nil {
+					fmt.Printf("insert block_num %d\tOK\n", increment)
+					increment++
+				}
 			}
-			fmt.Printf("insert block_num %d\tOK\n", increment)
-			increment++
 		case <-done:
 			return
 		}
