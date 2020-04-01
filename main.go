aa6783e31a6044f72e21b9d35c911d782dcee67c
Gar|k <garik.djan@gmail.com>
2020-04-01 13:34:40 +0300

    feat: const EPSt in period

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"flag"
 	"fmt"
 	"github.com/eoscanada/eos-go"
 	"github.com/gorilla/mux"
@@ -18,14 +17,17 @@ import (
 	"time"
 )
 
+/*
+Задача
+три минуты интервал ESPt от 100 до 500
+*/
+
 const (
 	contractAbiFileName = "contract.abi"
 	contractActionName  = "send"
 
-	flagIntervalValue = time.Millisecond * 500
-	flagIntervalUsage = "event creation interval"
-
 	waitConnectionToClose = 2 * time.Second
+	periodDuration        = 3 * time.Minute
 
 	sqlTruncate = "TRUNCATE TABLE chain.action_trace"
 	sqlInsert   = "INSERT INTO chain.action_trace(transaction_id, action_ordinal, act_name, act_data, block_num, receipt_global_sequence) VALUES ('A229C41BF5974D45E2EB11D9987B92E980C68AF9A0C170F71CFF868469EF3DC5',1,'send', $1, $2, $3)"
@@ -36,16 +38,26 @@ var (
 		prometheus.CounterOpts{
 			Name: "events_total",
 		})
+
+	conn *pgx.Conn
+	abi  *eos.ABI
 )
 
 func init() {
 	prometheus.MustRegister(eventsTotal)
 }
 
-func main() {
-	interval := flag.Duration("interval", flagIntervalValue, flagIntervalUsage)
-	flag.Parse()
+func insertEvent(ctx context.Context, increment int) error {
+	data, err := encodeSendAction(abi, 0)
+	if err != nil {
+		return err
+	}
 
+	_, err = conn.Exec(ctx, sqlInsert, data, increment, increment)
+	return err
+}
+
+func main() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("error loading .env file")
@@ -59,94 +71,93 @@ func main() {
 		os.Getenv("POSTGRES_DB"),
 	)
 
-	fmt.Printf("database: %s\n%s: %v\n", dataSource, flagIntervalUsage, *interval)
+	router := mux.NewRouter()
+	router.Handle("/metrics", promhttp.Handler())
 
-	var (
-		conn *pgx.Conn
-		abi  *eos.ABI
-	)
+	server := &http.Server{Addr: os.Getenv("METRICS_ADDR"), Handler: router}
 
 	abi, err = loadAbiFromFile(contractAbiFileName)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	conn, err = pgx.Connect(context.Background(), dataSource)
+	mainContext := context.Background()
+	parentContext, parentCancel := context.WithCancel(mainContext)
+	conn, err = pgx.Connect(parentContext, dataSource)
 	if err != nil {
 		log.Fatal(err)
 	}
+	log.Printf("connected to database: %s", dataSource)
 
-	startInterval := float64(2000)
-	ticker := time.NewTicker(time.Duration(startInterval) * time.Millisecond)
-	counter := 1.0
-
-	ctx, cancel := context.WithTimeout(context.Background(), waitConnectionToClose)
+	waitShutdownContext, cancelWaitShutdown := context.WithTimeout(mainContext, waitConnectionToClose)
 	defer func() {
-		ticker.Stop()
-		if err := conn.Close(context.Background()); err != nil {
-			log.Print(err)
-		}
-
-		cancel()
+		conn.Close(parentContext)
+		cancelWaitShutdown()
+		parentCancel()
+		log.Println("database connection closed")
 	}()
 
 	// truncate table
-	_, err = conn.Exec(context.Background(), sqlTruncate)
+	_, err = conn.Exec(parentContext, sqlTruncate)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Print("truncate table action_trace: OK\n\n")
+	log.Println("truncate table action_trace")
 
 	done := make(chan os.Signal, 1)
 	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 
-	router := mux.NewRouter()
-	router.Handle("/metrics", promhttp.Handler())
-
-	server := &http.Server{Addr: os.Getenv("METRICS_ADDR"), Handler: router}
-
 	go func() {
+		log.Printf("start listen HTTP on %s", server.Addr)
 		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 			log.Fatal(err)
 		}
 	}()
 
+	intervalDurations := [...]time.Duration{time.Second / 100, time.Second / 200, time.Second / 300, time.Second / 400, time.Second / 500}
+
+	periodTicker := time.NewTicker(periodDuration)
+	periodCounter := 0
+
+	intervalTicker := time.NewTicker(intervalDurations[periodCounter])
+	log.Printf("start interval with %v", intervalDurations[periodCounter])
+
 	increment := 0
 
+loop:
 	for {
 		select {
-		case <-ticker.C:
-			var data []byte
-			data, err = encodeSendAction(abi, 0)
-			if err != nil {
-				log.Println(err)
-				break
-			}
-
-			if _, err := conn.Exec(context.Background(), sqlInsert, data, increment, increment); err == nil {
-				duration := time.Duration(startInterval/counter) * time.Millisecond
-
-				if duration <= 0 {
-					duration = time.Nanosecond
-					counter = 1.0
-				}
-
-				log.Printf("insert block_num %d, next duration %v\tOK\n", increment, duration)
-
-				ticker.Stop()
-				ticker = time.NewTicker(duration)
-
-				counter++
-				increment++
-
-				eventsTotal.Inc()
-			}
-
 		case <-done:
-			if err := server.Shutdown(ctx); err != nil {
+			intervalTicker.Stop()
+			periodTicker.Stop()
+			break loop
+		case <-intervalTicker.C:
+			if err := insertEvent(parentContext, increment); err != nil {
 				log.Fatal(err)
 			}
-			return
+			log.Printf("insert block_num %d", increment)
+			increment++
+			eventsTotal.Inc()
+
+		case <-periodTicker.C:
+			log.Printf("period %v cancel", periodDuration)
+			intervalTicker.Stop()
+
+			periodCounter++
+
+			if periodCounter == len(intervalDurations) {
+				periodTicker.Stop()
+				break loop
+			}
+
+			intervalTicker = time.NewTicker(intervalDurations[periodCounter])
+			log.Printf("start interval with %v", intervalDurations[periodCounter])
 		}
 	}
+
+	if err := server.Shutdown(waitShutdownContext); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("listening stopped")
 }
